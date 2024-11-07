8233496e786d9628e7f2243139f323c4b92db52c
Akin Holo <holoakinseloyin@gmail.com>
2024-11-08 00:49:55 +0100

    Learn Basic Code
    Write basic code to change two different type of image and also prompt user to enter name

--- main.js
+++ main.js
@@ -11,3 +11,27 @@ myImage.onclick = () => {
 
     }
 }
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("please enter your name");
+    if(!myName) {
+        setUserName();
+    } else {
+    localStorage.setItem("Name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;  
+    }
+}
+  if (!localStorage.getItem("name")) {
+     setUserName();
+      } else {
+        const storedName = localStorage.getItem("name");
+         myHeading.textContent = `Mozilla is cool, ${storedName}`;
+      }
+
+      
+myButton.onclick = () => {
+    setUserName();
+}
